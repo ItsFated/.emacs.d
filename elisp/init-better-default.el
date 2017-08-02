e04bb1813e6d7c51815b7c9a7d201dee33777f9f
Jason <384388433@qq.com>
2017-08-02 18:52:05 +0800

    修改编码设置

--- elisp/init-better-default.el
+++ elisp/init-better-default.el
@@ -75,14 +75,14 @@
 (set-default buffer-file-coding-system 'utf8)
 (set-default-coding-systems 'utf-8)
 ;; 解决粘贴中文出现乱码的问题
-(set-clipboard-coding-system 'utf-8)
+;; (set-clipboard-coding-system 'utf-8)
 ;; 终端中文乱码
-(set-terminal-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
-(setq default-process-coding-system '(utf-8 . utf-8))
+;; (set-terminal-coding-system 'utf-8)
+;; (modify-coding-system-alist 'process "*" 'utf-8)
+;; (setq default-process-coding-system '(utf-8 . utf-8))
 ;; 解决文件目录的中文名乱码
-(setq-default pathname-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
+;; (setq-default pathname-coding-system 'utf-8)
+;; (set-file-name-coding-system 'utf-8)
 ;; 解决 Shell Mode(cmd) 下中文乱码问题
 ;; (defun change-shell-mode-coding ()
 ;;   (progn
