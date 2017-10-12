6fafadfc60a1ecc9baf87986f8bfdb9f84a18d30
Jason <384388433@qq.com>
2017-10-12 09:50:53 +0800

    .

--- elisp/init-ui.el
+++ elisp/init-ui.el
@@ -15,9 +15,16 @@
 ;; (toggle-frame-fullscreen)
 
 ;; 设置字体
-(set-frame-font "YaHei Consolas Hybrid 10")
-
+;; (set-frame-font "YaHei Consolas Hybrid 10")
+;; Setting English Font
+(set-face-attribute
+ 'default nil :font "Consolas 10")
 ;; 避免保存的时候出现选择字符集的提示，MiniBuffer显示里Buffers的速度提升
-(set-fontset-font "fontset-default" 'unicode '("YaHei Consolas Hybrid" . "unicode-ttf"))
+(set-fontset-font "fontset-default" 'unicode '("Microsoft Yahei" . "unicode-ttf"))
+;; Chinese Font
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                    (font-spec :family "Microsoft Yahei" :size 14)))
 
 (provide 'init-ui)
